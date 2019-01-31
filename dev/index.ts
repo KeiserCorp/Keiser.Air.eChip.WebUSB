cf8b82faa48622ffea90c58b4805e2988d87ac1e
Zach Cardoza <2280384+bayssmekanique@users.noreply.github.com>
2019-01-31 11:18:41 -0800

    Adds better test env

--- dev/index.ts
+++ dev/index.ts
@@ -1,11 +1,14 @@
 import EChipReaderWatcher from '../src/echipReaderWatcher'
 import EChipReader from '../src/echipReader'
 import EChip from '../src/echip'
+import SyntaxHighlight from './syntax'
 
 document.addEventListener('DOMContentLoaded', event => {
   const echipReaderWatcher = new EChipReaderWatcher()
   let echipReaders: Array<EChipReader> = []
-  const connectButton = document.querySelector('#connect')
+  const connectButton = document.querySelector('#connect') as HTMLInputElement
+  const disconnectButton = document.querySelector('#disconnect') as HTMLInputElement
+  const keyField = document.querySelector('#key')
   const outputField = document.querySelector('#output')
 
   window.addEventListener('onunload', event => {
@@ -24,20 +27,62 @@ document.addEventListener('DOMContentLoaded', event => {
     })
   }
 
-  const processEchip = async (echip: EChip) => {
+  if (disconnectButton) {
+    disconnectButton.addEventListener('click', async () => {
+      try {
+        await echipReaderWatcher.stop()
+      } catch (error) {
+        console.error(error.message)
+      }
+    })
+  }
+
+  const disconnectEchip = () => {
+    if (keyField) {
+      keyField.innerHTML = ''
+    }
+
     if (outputField) {
       outputField.innerHTML = ''
-      outputField.innerHTML += echip.id + '\n'
+    }
+  }
+
+  const connectEchip = async (echip: EChip) => {
+    echip.onDisconnect(disconnectEchip)
+
+    if (keyField) {
+      keyField.innerHTML = echip.id
     }
 
     let data = await echip.getData()
     console.log(data)
+    if (outputField) {
+      outputField.innerHTML = SyntaxHighlight(data.machineData)
+    }
   }
 
   echipReaderWatcher.onConnect((echipReader) => {
+    echipReader.onDisconnect(() => {
+      if (disconnectButton) {
+        disconnectButton.disabled = true
+      }
+
+      if (connectButton) {
+        connectButton.disabled = false
+      }
+    })
+
+    if (disconnectButton) {
+      disconnectButton.disabled = false
+    }
+
+    if (connectButton) {
+      connectButton.disabled = true
+    }
+
     echipReaders.push(echipReader)
     echipReader.onEChipDetect((echip) => {
-      processEchip(echip)
+      connectEchip(echip)
     })
   })
 })
