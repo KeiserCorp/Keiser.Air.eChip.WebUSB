9fca07a2d7c1055178f9a5c397a533ca9b0a378c
Zach Cardoza <2280384+bayssmekanique@users.noreply.github.com>
2020-01-03 08:04:32 -0800

    Event sync (#41)
    * Postpones initialization
    * Removes isConnected property
    * Updates documentation
    
    Closes #40

--- src/chipReaderWatcher.ts
+++ src/chipReaderWatcher.ts
@@ -9,7 +9,6 @@ const CHIP_READER_PRODUCT_ID = 0x2490
 export class ChipReaderWatcher extends USBDevice {
   private onConnectEvent = new TypedEvent<ChipReader>()
   private onDisconnectEvent = new TypedEvent<WebUSBDevice>()
-  private connectedChipReaders: Array<ChipReader> = []
 
   constructor () {
     super(CHIP_READER_VENDOR_ID, CHIP_READER_PRODUCT_ID)
@@ -19,16 +18,10 @@ export class ChipReaderWatcher extends USBDevice {
     await Promise.all(this.connectedDevices.map(d => this.disconnected(d)))
   }
 
-  get isConnected () {
-    return this.connectedChipReaders.length > 0
-  }
-
-  getConnectedChipReaders () {
-    return this.connectedChipReaders.slice(0)
-  }
-
   onConnect (listener: Listener<ChipReader>): Disposable {
-    return this.onConnectEvent.on(listener)
+    const unsubscribe = this.onConnectEvent.on(listener)
+    this.initialize()
+    return unsubscribe
   }
 
   private onDisconnect (listener: Listener<WebUSBDevice>) {
@@ -39,7 +32,6 @@ export class ChipReaderWatcher extends USBDevice {
     await super.connected(device)
     const chipReader = new ChipReader(device, (l: Listener<WebUSBDevice>) => this.onDisconnect(l))
     await chipReader.claimed
-    this.connectedChipReaders.push(chipReader)
     this.onConnectEvent.emit(chipReader)
   }
 
