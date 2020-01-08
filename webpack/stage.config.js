d6679f0630a69620d4bb3698a89acdc0dc9b98b1
Zachary Cardoza <2280384+bayssmekanique@users.noreply.github.com>
2020-01-08 08:24:27 -0800

    Improves stage deployment

--- webpack/stage.config.js
+++ webpack/stage.config.js
@@ -1,4 +1,7 @@
+const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const STAGE = path.resolve(__dirname, '../stage')
 
 module.exports = {
   mode: 'production',
@@ -13,18 +16,19 @@ module.exports = {
     ]
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js']
-  },
-  devServer: {
-    contentBase: './dev'
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      'vue': 'vue/dist/vue.common.js'
+    }
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './dev/index.html'
-    }),
+    new HtmlWebpackPlugin({ template: './dev/index.html' }),
+    new CopyWebpackPlugin([{ from: './dev/index.html', to: path.join(STAGE, 'index.html') }]),
+    new CopyWebpackPlugin([{ from: './dev/style.css', to: path.join(STAGE, 'style.css') }])
   ],
   output: {
     filename: 'index.js',
+    path: STAGE,
     libraryTarget: 'umd',
     library: 'EChip'
   }
