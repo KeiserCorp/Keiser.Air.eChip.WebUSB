f450fc75e45ac1a5bac89863341d4340bac0d03c
Zachary Cardoza <2280384+bayssmekanique@users.noreply.github.com>
2020-03-14 09:30:22 -0700

    Removes .npmrc file from dist

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -3,7 +3,6 @@ const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const WriteJsonPlugin = require('write-json-webpack-plugin')
-const CreateFileWebpack = require('create-file-webpack')
 const DIST = path.resolve(__dirname, '../dist')
 const package = Object.assign(require('../package.json'), {
   main: 'index.js',
@@ -83,11 +82,6 @@ module.exports = {
     new CopyWebpackPlugin([{ from: 'types', to: 'types' }]),
     new CopyWebpackPlugin([{ from: 'README.md', to: 'README.md' }]),
     new CopyWebpackPlugin([{ from: 'LICENSE.md', to: 'LICENSE.md' }]),
-    new CreateFileWebpack({
-      path: './dist',
-      fileName: '.npmrc',
-      content: '//registry.npmjs.org/:_authToken=${NPM_TOKEN}'
-    }),
     new WriteJsonPlugin({
       object: package,
       filename: 'package.json',
